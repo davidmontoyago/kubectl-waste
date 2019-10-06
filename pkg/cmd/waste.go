611a5eda6c49a3d399eb8affed5ae7d7cb636251
David Montoya <dmontoya@thoughtworks.com>
2019-10-06 16:30:07 -0500

    dead code clean up

--- pkg/cmd/waste.go
+++ pkg/cmd/waste.go
@@ -19,17 +19,11 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/clientcmd/api"
-
-	metricsclientset "k8s.io/metrics/pkg/client/clientset/versioned"
-
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
+	metricsclientset "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 var (
@@ -40,8 +34,6 @@ var (
 	# find all wasteful pods in a namespace
 	%[1]s waste -n my-namespace
 `
-
-	errNoContext = fmt.Errorf("no context is currently set, use %q to select a new one", "kubectl config use-context <context>")
 )
 
 var (
@@ -52,17 +44,9 @@ var (
 type CommandOptions struct {
 	configFlags *genericclioptions.ConfigFlags
 
-	resultingContext     *api.Context
-	resultingContextName string
-
-	userSpecifiedCluster   string
-	userSpecifiedContext   string
-	userSpecifiedAuthInfo  string
 	userSpecifiedNamespace string
 
-	rawConfig      api.Config
-	listNamespaces bool
-	args           []string
+	args []string
 
 	clientset        *kubernetes.Clientset
 	metricsClientset *metricsclientset.Clientset
@@ -105,7 +89,7 @@ func NewCmd(streams genericclioptions.IOStreams) *cobra.Command {
 		},
 	}
 
-	cmd.Flags().BoolVar(&o.listNamespaces, "list", o.listNamespaces, "if true, print the list of all namespaces in the current KUBECONFIG")
+	// cmd.Flags().BoolVar(&o.myFlag, "flag-name", o.myFlag, "if true, do something")
 	o.configFlags.AddFlags(cmd.Flags())
 
 	return cmd
@@ -128,103 +112,16 @@ func (o *CommandOptions) Complete(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	o.rawConfig, err = o.configFlags.ToRawKubeConfigLoader().RawConfig()
-	if err != nil {
-		return err
-	}
-
 	o.userSpecifiedNamespace, err = cmd.Flags().GetString("namespace")
 	if err != nil {
 		return err
-	}
-	if len(args) > 0 {
-		if len(o.userSpecifiedNamespace) > 0 {
-			return fmt.Errorf("cannot specify both a --namespace value and a new namespace argument")
-		}
-
-		o.userSpecifiedNamespace = args[0]
-	}
-
-	// if no namespace argument or flag value was specified, then there
-	// is no need to generate a resulting context
-	if len(o.userSpecifiedNamespace) == 0 {
-		return nil
-	}
-
-	o.userSpecifiedContext, err = cmd.Flags().GetString("context")
-	if err != nil {
-		return err
-	}
-
-	o.userSpecifiedCluster, err = cmd.Flags().GetString("cluster")
-	if err != nil {
-		return err
-	}
-
-	o.userSpecifiedAuthInfo, err = cmd.Flags().GetString("user")
-	if err != nil {
-		return err
-	}
-
-	currentContext, exists := o.rawConfig.Contexts[o.rawConfig.CurrentContext]
-	if !exists {
-		return errNoContext
-	}
-
-	o.resultingContext = api.NewContext()
-	o.resultingContext.Cluster = currentContext.Cluster
-	o.resultingContext.AuthInfo = currentContext.AuthInfo
-
-	// if a target context is explicitly provided by the user,
-	// use that as our reference for the final, resulting context
-	if len(o.userSpecifiedContext) > 0 {
-		o.resultingContextName = o.userSpecifiedContext
-		if userCtx, exists := o.rawConfig.Contexts[o.userSpecifiedContext]; exists {
-			o.resultingContext = userCtx.DeepCopy()
-		}
-	}
-
-	// override context info with user provided values
-	o.resultingContext.Namespace = o.userSpecifiedNamespace
-
-	if len(o.userSpecifiedCluster) > 0 {
-		o.resultingContext.Cluster = o.userSpecifiedCluster
-	}
-	if len(o.userSpecifiedAuthInfo) > 0 {
-		o.resultingContext.AuthInfo = o.userSpecifiedAuthInfo
-	}
-
-	// generate a unique context name based on its new values if
-	// user did not explicitly request a context by name
-	if len(o.userSpecifiedContext) == 0 {
-		o.resultingContextName = generateContextName(o.resultingContext)
 	}
 
 	return nil
 }
 
-func generateContextName(fromContext *api.Context) string {
-	name := fromContext.Namespace
-	if len(fromContext.Cluster) > 0 {
-		name = fmt.Sprintf("%s/%s", name, fromContext.Cluster)
-	}
-	if len(fromContext.AuthInfo) > 0 {
-		cleanAuthInfo := strings.Split(fromContext.AuthInfo, "/")[0]
-		name = fmt.Sprintf("%s/%s", name, cleanAuthInfo)
-	}
-
-	return name
-}
-
 // Validate ensures that all required arguments and flag values are provided
 func (o *CommandOptions) Validate() error {
-	if len(o.rawConfig.CurrentContext) == 0 {
-		return errNoContext
-	}
-	if len(o.args) > 1 {
-		return fmt.Errorf("either one or no arguments are allowed")
-	}
-
 	return nil
 }
 
@@ -240,43 +137,4 @@ func (o *CommandOptions) Run() error {
 	}
 	printPods(foundPods)
 	return nil
-}
-
-func isContextEqual(ctxA, ctxB *api.Context) bool {
-	if ctxA == nil || ctxB == nil {
-		return false
-	}
-	if ctxA.Cluster != ctxB.Cluster {
-		return false
-	}
-	if ctxA.Namespace != ctxB.Namespace {
-		return false
-	}
-	if ctxA.AuthInfo != ctxB.AuthInfo {
-		return false
-	}
-
-	return true
-}
-
-// setNamespace receives a "desired" context state and determines if a similar context
-// is already present in a user's KUBECONFIG. If one is not, then a new context is added
-// to the user's config under the provided destination name.
-// The current context field is updated to point to the new context.
-func (o *CommandOptions) setNamespace(fromContext *api.Context, withContextName string) error {
-	if len(fromContext.Namespace) == 0 {
-		return fmt.Errorf("a non-empty namespace must be provided")
-	}
-
-	configAccess := clientcmd.NewDefaultPathOptions()
-
-	// determine if we have already saved this context to the user's KUBECONFIG before
-	// if so, simply switch the current context to the existing one.
-	if existingResultingCtx, exists := o.rawConfig.Contexts[withContextName]; !exists || !isContextEqual(fromContext, existingResultingCtx) {
-		o.rawConfig.Contexts[withContextName] = fromContext
-	}
-	o.rawConfig.CurrentContext = withContextName
-
-	fmt.Fprintf(o.Out, "namespace changed to %q\n", fromContext.Namespace)
-	return clientcmd.ModifyConfig(configAccess, o.rawConfig, true)
 }
